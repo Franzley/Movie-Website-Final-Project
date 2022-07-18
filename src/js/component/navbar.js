4b90cf0dbbc3da730a56100c2ab7824b05d2efcb
bonh0303 <fbonhomme9721@gmail.com>
2022-07-18 23:49:26 +0000

    Initial Commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,17 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">REEL REVIEWS</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+			<Link to="/login">
+					<button className="btn btn-primary">Join Now</button>
+				</Link>
+				<Link to="/login">
+					<button className="btn btn-primary">Login</button>
+				</Link>
+				<Link to="/watch">
+					<button className="btn btn-primary">WatchList</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/layout.js
+++ src/js/layout.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { Login } from "./views/login";
+import { SearchResults } from "./views/searchresults";
+import { WatchList } from "./views/watchlist";
+
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,11 +27,14 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/login">
+							<Login />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
+						<Route exact path="/search">
+							<SearchResults />
+						</Route>
+						<Route exact path="/watch">
+							<WatchList />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
