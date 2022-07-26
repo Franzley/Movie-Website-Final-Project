acda9aeba17971e1fa20875993c805b4b93e4e0d
bonh0303 <fbonhomme9721@gmail.com>
2022-07-26 20:34:23 +0000

    Temporary WatchList

--- src/js/firebase/firebaseConfig.js
+++ src/js/firebase/firebaseConfig.js
@@ -1,14 +1,35 @@
 import firebase from 'firebase/compat/app' 
 import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyD3cSjXaxlTKP9799HNGS1v90AU3Q_-KSY",
-    authDomain: "auth-development-88dd7.firebaseapp.com",
-    projectId: "auth-development-88dd7",
-    storageBucket: "auth-development-88dd7.appspot.com",
-    messagingSenderId: "137778238011",
-    appId: "1:137778238011:web:3b6ea616ee31797cc0c21e"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }) 
+
+// // Initialize Cloud Firestore and get a reference to the service
+// const db = firebase.firestore();
+// db.collection("users").add({
+//   first: "Ada",
+//   last: "Lovelace",
+//   born: 1815
+// })
+// .then((docRef) => {
+//   console.log("Document written with ID: ", docRef.id);
+// })
+// .catch((error) => {
+//   console.error("Error adding document: ", error);
+// });
+
+// db.collection("users").get().then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//       console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+//   });
+// });
 
 export const auth = app.auth()
 export default app
