85f94c075767f1fe4bf86890dac5dbdb2a7cc44e
c-chimie <cchimie303@gmail.com>
2022-07-30 00:07:19 +0000

    made css edits

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useAuth } from "../firebase/AuthContext";
 import { useHistory } from "react-router-dom";
-import logo from "/workspace/Movie-Website-Final-Project/src/images/logo.png";
+import logo from "../../images/logo.png";
 
 export const Navbar = () => {
   const [error, setError] = useState("");
